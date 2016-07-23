336b6067cff55ffe65c8dbd75666f51a4d6be113
Erik Pearson <erik@adaptations.com>
2016-07-23 09:52:11 -0700

    fix object property setting bug: Notice: Array to string conversion in /var/www/www_a1suninc_com/build/php/lib/vendor/sag/sag/src/httpAdapters/SagCURLHTTPAdapter.php on line 124 updated formatting removed end tag from source

--- src/SagFileCache.php
+++ src/SagFileCache.php
@@ -1,17 +1,18 @@
 <?php
+
 /*
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 
 require_once('SagCache.php');
 require_once('SagException.php');
@@ -30,121 +31,121 @@ require_once('SagException.php');
  * @version %VERSION%
  */
 class SagFileCache extends SagCache {
-  private static $fileExt = ".sag";
 
-  private $fsLocation;
+    private static $fileExt = ".sag";
+    private $fsLocation;
 
-  /**
-   * @param string $location The file system path to the directory that should
-   * be used to store the cache files. The local system's temp directory is
-   * used by default.
-   * @return SagFileCache
-   */
-  public function __construct($location) {
-    if(!is_dir($location)) {
-      throw new SagException("The provided cache location is not a directory.");
-    }
-
-    if(!is_readable($location) || !is_writable($location)) {
-      throw new SagException("Insufficient privileges to the supplied cache directory.");
-    }
-
-    parent::__construct();
-
-    $this->fsLocation = rtrim($location, "/ \t\n\r\0\x0B");
-
-    /* 
-     * Just update - don't freak out if the size isn't right, as the user might
-     * update it to non-default, they might not do anything with the cache,
-     * they might clean it themselves, etc. give them time. We'll freak when we
-     * add.
+    /**
+     * @param string $location The file system path to the directory that should
+     * be used to store the cache files. The local system's temp directory is
+     * used by default.
+     * @return SagFileCache
      */
-    foreach(glob($this->fsLocation."/*".self::$fileExt) as $file) {
-      self::addToSize(filesize($file));
-    }
-  }   
+    public function __construct($location) {
+        if (!is_dir($location)) {
+            throw new SagException("The provided cache location is not a directory.");
+        }
 
-  /**
-   * Generates the full filename/path that would be used for a given URL's
-   * cache object.
-   *
-   * @param string $url The URL for the cached item.
-   * @return string
-   */
-  public function makeFilename($url) {
-    return "$this->fsLocation/".self::makeKey($url).self::$fileExt;
-  }
+        if (!is_readable($location) || !is_writable($location)) {
+            throw new SagException("Insufficient privileges to the supplied cache directory.");
+        }
 
-  public function set($url, &$item) {
-    if(empty($url)) {
-      throw new SagException('You need to provide a URL to cache.');
-    }
+        parent::__construct();
 
-    if(!parent::mayCache($item)) {
-      return false;
-    }
+        $this->fsLocation = rtrim($location, "/ \t\n\r\0\x0B");
 
-    $serialized = json_encode($item);
-    $target = self::makeFilename($url);
-
-    // If it already exists, then remove the old version but keep a copy
-    if(is_file($target)) {
-      $oldCopy = self::get($url);
-      self::remove($url);
+        /*
+         * Just update - don't freak out if the size isn't right, as the user might
+         * update it to non-default, they might not do anything with the cache,
+         * they might clean it themselves, etc. give them time. We'll freak when we
+         * add.
+         */
+        foreach (glob($this->fsLocation . "/*" . self::$fileExt) as $file) {
+            self::addToSize(filesize($file));
+        }
     }
 
-    $fh = fopen($target, "w"); //in case self::remove() didn't get it?
-
-    fwrite($fh, $serialized, strlen($serialized)); //don't throw up if we fail - we're not mission critical
-    self::addToSize(filesize($target));
-
-    fclose($fh);
-
-    // Only return the $oldCopy if it exists
-    return (isset($oldCopy) && is_object($oldCopy)) ? $oldCopy : true;
-  }
-
-  public function get($url) {
-    $target = self::makeFilename($url);
-
-    if(!is_file($target)) {
-      return null;
+    /**
+     * Generates the full filename/path that would be used for a given URL's
+     * cache object.
+     *
+     * @param string $url The URL for the cached item.
+     * @return string
+     */
+    public function makeFilename($url) {
+        return "$this->fsLocation/" . self::makeKey($url) . self::$fileExt;
     }
 
-    if(!is_readable($target)) {
-      throw new SagException("Could not read the cache file for $url at $target - please check its permissions.");
+    public function set($url, &$item) {
+        if (empty($url)) {
+            throw new SagException('You need to provide a URL to cache.');
+        }
+
+        if (!parent::mayCache($item)) {
+            return false;
+        }
+
+        $serialized = json_encode($item);
+        $target = self::makeFilename($url);
+
+        // If it already exists, then remove the old version but keep a copy
+        if (is_file($target)) {
+            $oldCopy = self::get($url);
+            self::remove($url);
+        }
+
+        $fh = fopen($target, "w"); //in case self::remove() didn't get it?
+
+        fwrite($fh, $serialized, strlen($serialized)); //don't throw up if we fail - we're not mission critical
+        self::addToSize(filesize($target));
+
+        fclose($fh);
+
+        // Only return the $oldCopy if it exists
+        return (isset($oldCopy) && is_object($oldCopy)) ? $oldCopy : true;
     }
 
-    return json_decode(file_get_contents($target));
-  }
+    public function get($url) {
+        $target = self::makeFilename($url);
 
-  public function remove($url) {
-    $target = $this->makeFilename($url);
-    return self::removeFile($target);
-  }
+        if (!is_file($target)) {
+            return null;
+        }
 
-  public function clear() {
-    $part = false;
+        if (!is_readable($target)) {
+            throw new SagException("Could not read the cache file for $url at $target - please check its permissions.");
+        }
 
-    foreach(glob($this->fsLocation."/*".self::$fileExt) as $file) {
-      if(!self::removeFile($file)) {
-        $part = true;
-      }
-    } 
-
-    return !$part;
-  }
-
-  private function removeFile($path) {
-    $size = filesize($path);
-
-    if(!unlink($path)) {
-      return false;
+        return json_decode(file_get_contents($target));
     }
 
-    self::addToSize(-$size);
+    public function remove($url) {
+        $target = $this->makeFilename($url);
+        return self::removeFile($target);
+    }
 
-    return true;
-  }
-} 
-?>
+    public function clear() {
+        $part = false;
+
+        foreach (glob($this->fsLocation . "/*" . self::$fileExt) as $file) {
+            if (!self::removeFile($file)) {
+                $part = true;
+            }
+        }
+
+        return !$part;
+    }
+
+    private function removeFile($path) {
+        $size = filesize($path);
+
+        if (!unlink($path)) {
+            return false;
+        }
+
+        self::addToSize(-$size);
+
+        return true;
+    }
+
+}
